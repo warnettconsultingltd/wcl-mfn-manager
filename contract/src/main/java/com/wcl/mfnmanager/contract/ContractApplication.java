4f029ee1415a8b9ce5b2f2d34a6ed8727fca87f1
WCL - Martin Warnett <coding.warnettconsultingltd@gmail.com>
2021-06-20 10:51:19 +0100

    Added basic Spring Boot application without controller

---
+++ contract/src/main/java/com/wcl/mfnmanager/contract/ContractApplication.java
@@ -0,0 +1,13 @@
+package com.wcl.mfnmanager.contract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.wcl.mfnmanager.contract.ContractApplication.class, args);
+	}
+
+}
