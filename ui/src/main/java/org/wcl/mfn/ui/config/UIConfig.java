7dfeed6c5a6576a341890b880f517a93bcf90b56
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-06-18 21:07:23 +0100

    Initial design for Menu Bar with a Drop down menu for Tools.
    Further styling is required.

--- ui/src/main/java/org/wcl/mfn/ui/config/UIConfig.java
+++ ui/src/main/java/org/wcl/mfn/ui/config/UIConfig.java
@@ -1,7 +1,14 @@
 package org.wcl.mfn.ui.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class UIConfig {
+public class UIConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
 }
--- ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+//@RequestMapping("/")
 public class HomeController {
     @Value("${page.home.title}")
     private String title;
+
     private static final String HOME_PAGE = "pages/home/home";
-    @GetMapping
+    @GetMapping("/")
     public String homePage(Model model) {
         model.addAttribute("page_title", title);
         return HOME_PAGE;
