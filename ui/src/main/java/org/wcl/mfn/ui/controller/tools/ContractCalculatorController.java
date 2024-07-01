face4d7d2c80f48e9f17908caf86b2a5a72c4d76
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-01 23:10:50 +0100

    Amended Contract Calculator Controller to add a suggested contracts model attribute consisting blank contracts.

--- ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.wcl.mfn.ui.model.SuggestedYearContract;
+
+import java.util.List;
 
 @Controller
 public class ContractCalculatorController {
@@ -12,9 +15,26 @@ public class ContractCalculatorController {
 
     private static final String CONTRACT_CALCULATOR_PAGE = "pages/tools/contract-calculator";
 
-    @GetMapping("/contract_calculator")
+    @GetMapping("/contract-calculator")
     public String contractCalculatorPage(Model model) {
         model.addAttribute("page_title", title);
+        model.addAttribute("suggestedContracts",generateBlankContracts());
+
         return CONTRACT_CALCULATOR_PAGE;
     }
+
+    private List<SuggestedYearContract> generateBlankContracts() {
+       return List.of(new SuggestedYearContract(2,0,0,
+                            List.of(0,0),0,0),
+                      new SuggestedYearContract(3,0,0,
+                            List.of(0,0, 0),0,0),
+                      new SuggestedYearContract(4,0,0,
+                            List.of(0,0, 0, 0),0,0),
+                      new SuggestedYearContract(5,0,0,
+                            List.of(0,0, 0, 0, 0),0,0),
+                      new SuggestedYearContract(6,0,0,
+                            List.of(0,0, 0, 0, 0, 0),0,0));
+    }
 }
+
+
