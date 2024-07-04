e881137e9052f52150e4141a8c558a2088a08e55
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-04 18:37:50 +0100

    Removed commented out code

--- ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/")
 public class HomeController {
     @Value("${page.home.title}")
     private String title;
