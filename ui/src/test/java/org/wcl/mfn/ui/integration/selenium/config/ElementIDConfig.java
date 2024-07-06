7dc1430cb01cbaa9a946267f36dbb113d899b5e8
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-06 22:16:02 +0100

    Added Config class to store immutable constants for Element ID values for testing purposes.

---
+++ ui/src/test/java/org/wcl/mfn/ui/integration/selenium/config/ElementIDConfig.java
@@ -0,0 +1,33 @@
+package org.wcl.mfn.ui.integration.selenium.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-test.properties")
+public class ElementIDConfig {
+    private final String navbar;
+    private final String navbarHome;
+    private final String navbarContractCalculator;
+
+    public ElementIDConfig(@Value("${mfn.navbar}") String navbar,
+                           @Value("${mfn.navbar.home}") String navbarHome,
+                           @Value("${mfn.navbar.contract-calculator}") String navbarContractCalculator) {
+        this.navbar = navbar;
+        this.navbarHome = navbarHome;
+        this.navbarContractCalculator = navbarContractCalculator;
+    }
+
+    public String navbar() {
+        return navbar;
+    }
+
+    public String navbarHome() {
+        return navbarHome;
+    }
+
+    public String navbarContractCalculator() {
+        return navbarContractCalculator;
+    }
+}
