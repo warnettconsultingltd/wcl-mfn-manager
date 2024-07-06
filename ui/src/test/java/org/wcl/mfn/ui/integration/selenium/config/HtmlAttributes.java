8af4eac62107268b4a5ee4df51452163d5cdeb23
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-06 21:54:24 +0100

    Created Config class to provide immutable storage of HTML attribute constants used in the app.

---
+++ ui/src/test/java/org/wcl/mfn/ui/integration/selenium/config/HtmlAttributes.java
@@ -0,0 +1,17 @@
+package org.wcl.mfn.ui.integration.selenium.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HtmlAttributes {
+    private final String link;
+
+    public HtmlAttributes(@Value("${attribute.link}") String link) {
+        this.link = link;
+    }
+
+    public String link() {
+        return link;
+    }
+}
