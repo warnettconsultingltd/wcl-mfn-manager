de8dc850d693999cc5947eef6a5ad95b1c3df0bf
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-06 21:58:09 +0100

    Corrected to add property source in.

--- ui/src/test/java/org/wcl/mfn/ui/integration/selenium/config/HtmlAttributes.java
+++ ui/src/test/java/org/wcl/mfn/ui/integration/selenium/config/HtmlAttributes.java
@@ -2,8 +2,10 @@ package org.wcl.mfn.ui.integration.selenium.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application-test.properties")
 public class HtmlAttributes {
     private final String link;
 
