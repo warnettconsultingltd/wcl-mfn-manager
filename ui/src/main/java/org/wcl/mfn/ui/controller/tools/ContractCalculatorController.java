434edf800340ea4c23d5d1020e049c1cabeb7f01
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-09 13:03:22 +0100

    Removed blank lines

--- ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.wcl.mfn.ui.model.SuggestedYearContract;
+import org.wcl.mfn.ui.model.tools.contractcalculator.SuggestedYearContract;
 
 import java.util.List;
 
