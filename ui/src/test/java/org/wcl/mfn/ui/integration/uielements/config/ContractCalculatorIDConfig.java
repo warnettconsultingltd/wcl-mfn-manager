5426d867a6ff89e21207ef351d6ab876d06022f7
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-19 22:39:33 +0100

    Extracted element IDs

--- ui/src/test/java/org/wcl/mfn/ui/integration/uielements/config/ContractCalculatorIDConfig.java
+++ ui/src/test/java/org/wcl/mfn/ui/integration/uielements/config/ContractCalculatorIDConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application-test.properties")
+@PropertySource("classpath:ui.properties")
 public class ContractCalculatorIDConfig {
     @Value("${mfn.navbar}")
     private String navbar;
--- ui/src/test/java/org/wcl/mfn/ui/integration/uielements/config/ElementIDConfig.java
+++ ui/src/test/java/org/wcl/mfn/ui/integration/uielements/config/ElementIDConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application-test.properties")
+@PropertySource("classpath:ui.properties")
 public class ElementIDConfig {
     private final String navbar;
     private final String navbarHome;
