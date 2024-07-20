853137b340e23e8ff532b7672e18d486d3c7f1b1
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-20 13:07:21 +0100

    Amended controllers to use newly created config files; the URLs however have to be present within these classes.

--- ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/HomeController.java
@@ -1,21 +1,30 @@
 package org.wcl.mfn.ui.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.wcl.mfn.config.ui.common.PageTitleConfig;
+import org.wcl.mfn.config.ui.mvc.*;
 
 @Controller
+@PropertySource({"classpath:url.properties"})
 public class HomeController {
+    @Autowired
+    private PageTitleConfig pageTitleConfig;
+
+    @Autowired
+    private ModelConfig modelConfig;
+
+    @Autowired
+    private ViewConfig viewConfig;
+
     @GetMapping("#{'${url.home}'}")
-    public String homePage(Model model,
-                           @Value("${common.view.attribute.page-title}") String pageTitleAttribute,
-                           @Value("${page.home.title}") String title,
-                           @Value("${view.home}") String viewName){
+    public String homePage(Model model) {
+        model.addAttribute(modelConfig.pageTitle(), pageTitleConfig.homePageTitle());
 
-        model.addAttribute(pageTitleAttribute, title);
-
-        return viewName;
+        return viewConfig.contractCalculatorView();
     }
 
 }
--- ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
+++ ui/src/main/java/org/wcl/mfn/ui/controller/tools/ContractCalculatorController.java
@@ -1,30 +1,40 @@
 package org.wcl.mfn.ui.controller.tools;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.wcl.mfn.config.ui.common.PageTitleConfig;
+import org.wcl.mfn.config.ui.mvc.*;
+import org.wcl.mfn.config.ui.tools.ContractCalculatorConfig;
 import org.wcl.mfn.ui.model.tools.contractcalculator.SuggestedYearContract;
 
 import java.util.List;
 
 @Controller
-@PropertySource("url.properties")
+@PropertySource({"classpath:url.properties"})
 public class ContractCalculatorController {
 
-    @GetMapping("#{'${url.tools.contract-calculator}'}")
-    public String contractCalculatorPage(Model model,
-                                         @Value("${common.view.attribute.page-title}") String pageTitleAttribute,
-                                         @Value("${page.contract-calculator.title}") String title,
-                                         @Value("${contract-calculator.view.attribute.contract-parameters}") String contractParameters,
-                                         @Value("${contract-calculator.view.attribute.suggested-contracts}") String suggestedContracts,
-                                         @Value("${view.tools.contract-calculator}") String viewName) {
-        model.addAttribute(pageTitleAttribute, title);
-        model.addAttribute(contractParameters, generateBlankContractParameters());
-        model.addAttribute(suggestedContracts,generateBlankContracts());
+    @Autowired
+    private PageTitleConfig pageTitleConfig;
 
-        return viewName;
+    @Autowired
+    private ViewConfig viewConfig;
+
+    @Autowired
+    private ModelConfig modelConfig;
+
+    @Autowired
+    private ContractCalculatorConfig contractCalculatorConfig;
+
+    @GetMapping("#{'${url.tools.contract-calculator}'}")
+    public String contractCalculatorPage(Model model) {
+        model.addAttribute(modelConfig.pageTitle(), pageTitleConfig.contractCalculatorPageTitle());
+        model.addAttribute(contractCalculatorConfig.contractParameters(), generateBlankContractParameters());
+        model.addAttribute(contractCalculatorConfig.suggestedContracts(),generateBlankContracts());
+
+        return viewConfig.contractCalculatorView();
     }
 
     private List<SuggestedYearContract> generateBlankContracts() {
