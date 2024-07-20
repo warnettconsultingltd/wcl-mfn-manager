96056e636f125c4c339fa0bdb683c85b640670ae
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-20 11:38:23 +0100

    Moved Resource resolver class

--- config/src/main/java/org/wcl/mfn/config/ui/UIConfigResourceResolver.java
+++ config/src/main/java/org/wcl/mfn/config/ui/UIConfigResourceResolver.java
@@ -1,14 +1,16 @@
-package org.wcl.mfn.ui.config;
+package org.wcl.mfn.config.ui;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class UIConfig implements WebMvcConfigurer {
+public class UIConfigResourceResolver implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("classpath:/static/css/");
     }
+
+
 }
