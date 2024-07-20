aa5ba0171c603a073d03eb411f1aad995a51c8c7
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-20 11:36:21 +0100

    Extracted Model attribute properties

---
+++ config/src/main/java/org/wcl/mfn/config/ui/mvc/ModelConfig.java
@@ -0,0 +1,16 @@
+package org.wcl.mfn.config.ui.mvc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("ui/mvc/model.properties")
+public class ModelConfig {
+    @Value("${common.view.attribute.page-title}")
+    private String pageTitle;
+
+    public String pageTitle() {
+        return pageTitle;
+    }
+}
