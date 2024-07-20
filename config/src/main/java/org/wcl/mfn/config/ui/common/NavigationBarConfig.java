56d3bac1edeb05c8c60d7cfec4abe90cc62fc9f2
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-20 11:21:26 +0100

    Corrected PropertySource value to correct proeprties file.

--- config/src/main/java/org/wcl/mfn/config/ui/common/NavigationBarConfig.java
+++ config/src/main/java/org/wcl/mfn/config/ui/common/NavigationBarConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@PropertySource("ui/ui.properties")
+@PropertySource("common/navigation-bar.properties")
 public class NavigationBarConfig {
     @Value("${mfn.navbar}")
     private String navigationBarId;
