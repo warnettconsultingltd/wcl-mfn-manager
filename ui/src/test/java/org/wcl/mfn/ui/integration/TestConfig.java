99ddefd792b2ad007e75b7ec39c7affabdd6e3ba
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-31 00:06:49 +0100

    Added generic TestConfig configuration file to include all in main app packages

---
+++ ui/src/test/java/org/wcl/mfn/ui/integration/TestConfig.java
@@ -0,0 +1,9 @@
+package org.wcl.mfn.ui.integration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.wcl.mfn")
+public class TestConfig {
+}
