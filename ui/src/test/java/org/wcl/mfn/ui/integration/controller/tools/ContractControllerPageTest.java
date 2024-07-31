1ded777489b8dcdf4b278d6a2ce128d59913f7b7
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-07-31 01:01:44 +0100

    Converted test to use new config

--- ui/src/test/java/org/wcl/mfn/ui/integration/controller/tools/ContractControllerPageTest.java
+++ ui/src/test/java/org/wcl/mfn/ui/integration/controller/tools/ContractControllerPageTest.java
@@ -8,36 +8,38 @@ import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfigurati
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.wcl.mfn.config.ui.common.PageTitleConfig;
+import org.wcl.mfn.config.ui.mvc.*;
+import org.wcl.mfn.config.ui.tools.ContractCalculatorConfig;
+import org.wcl.mfn.config.url.UrlConfig;
 import org.wcl.mfn.ui.controller.tools.ContractCalculatorController;
 import org.wcl.mfn.ui.utilities.NavigationBarChecker;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = ContractCalculatorController.class)
-@ContextConfiguration(classes = ContractCalculatorController.class)
-@ImportAutoConfiguration(ThymeleafAutoConfiguration.class)
+@WebMvcTest(ContractCalculatorController.class)
+@ContextConfiguration(classes = {ContractCalculatorController.class, ContractCalculatorConfig.class, PageTitleConfig.class, ModelConfig.class, ViewConfig.class, UrlConfig.class})
 public class ContractControllerPageTest {
+    @Autowired
+    private UrlConfig urlConfig;
 
-    @Value("${url.tools.contract-calculator}")
-    private String url;
-
-    @Value("${view.tools.contract-calculator}")
-    private String viewName;
+    @Autowired
+    private ViewConfig viewConfig;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void givenServerRunning_whenContractCalculatorURLInvoked_thenHomePageRendered() throws Exception {
-        mockMvc.perform(get(url))
+        mockMvc.perform(get(urlConfig.contractCalculatorUrl()))
                 .andExpect(status().isOk())
-                .andExpect(view().name(viewName));
+                .andExpect(view().name(viewConfig.contractCalculatorView()));
     }
 
     @Test
     public void givenServerRunning_whenHomePageDisplayed_thenNavigationBarExists() throws Exception {
-        final var mvcResult = mockMvc.perform(get(url)).andReturn();
+        final var mvcResult = mockMvc.perform(get(urlConfig.contractCalculatorUrl())).andReturn();
 
         NavigationBarChecker.checkNavigationBarContents(mvcResult.getResponse().getContentAsString());
     }
