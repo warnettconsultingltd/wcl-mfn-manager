bbe5e78af071abbd8f25e50bf2665186f0eeef37
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-08-12 19:47:31 +0100

    Added in service class

--- api/src/main/java/org/wcl/mfn/api/controller/tools/ContractCalculatorController.java
+++ api/src/main/java/org/wcl/mfn/api/controller/tools/ContractCalculatorController.java
@@ -1,18 +1,26 @@
 package org.wcl.mfn.api.controller.tools;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.wcl.mfn.api.entities.tools.*;
+import org.wcl.mfn.entities.contract.calculator.*;
+import org.wcl.mfn.service.tools.ContractCalculatorService;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 public class ContractCalculatorController implements ContractCalculatorApi {
+    private ContractCalculatorService service;
+
+    ContractCalculatorController(@Autowired ContractCalculatorService service) {
+        this.service = service;
+    }
 
     @Override
     public ResponseEntity<List<SuggestedContract>> contractCalculatorPost(
             @Valid @RequestBody RequestedRemuneration requestedRemuneration) {
-        return ResponseEntity.ok(new ArrayList<>());
+        return ResponseEntity.ok(service.suggestedContracts(requestedRemuneration.salary(),
+                                                            requestedRemuneration.bonus()));
     }
 }
