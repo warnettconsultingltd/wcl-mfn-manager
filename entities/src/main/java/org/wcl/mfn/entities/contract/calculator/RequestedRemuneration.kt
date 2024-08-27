1d1e606d3f724d7224c67bbbf1dae8f2ea355108
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-08-27 15:43:34 +0100

    Converted to Kotlin.

--- entities/src/main/java/org/wcl/mfn/entities/contract/calculator/RequestedRemuneration.kt
+++ entities/src/main/java/org/wcl/mfn/entities/contract/calculator/RequestedRemuneration.kt
@@ -1,21 +1,8 @@
-package org.wcl.mfn.entities.contract.calculator;
+package org.wcl.mfn.entities.contract.calculator
 
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-
-public record RequestedRemuneration(
-        @Schema(
-                name = "salary",
-                description = "The requested salary",
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        @JsonProperty("salary") Integer salary,
-        @Schema(
-                name = "bonus",
-                description = "The requested bonus",
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        @JsonProperty("bonus") Integer bonus) {}
+@JvmRecord
+data class RequestedRemuneration(
+    val salary: Int,
+    val bonus: Int
+)
 
--- entities/src/main/java/org/wcl/mfn/entities/contract/calculator/YearlyFigure.kt
+++ entities/src/main/java/org/wcl/mfn/entities/contract/calculator/YearlyFigure.kt
@@ -1,33 +1,10 @@
-package org.wcl.mfn.entities.contract.calculator;
+package org.wcl.mfn.entities.contract.calculator
 
+import jakarta.validation.constraints.*
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-
-public record YearlyFigure(
-        @Schema(
-                name = "year",
-                description = "The year of this suggested contract",
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        @JsonProperty("year")
-        @Min(1L)
-        @Max(6L) Integer year,
-
-        @Schema(
-                name = "salary",
-                description = "The salary for this year of the suggested contract",
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        @JsonProperty("salary") Integer salary,
-
-
-        @Schema(
-                name = "yearlyRemuneration",
-                description = "The yearly remuneration for this year of the suggested contract",
-                requiredMode = RequiredMode.NOT_REQUIRED
-        )
-        @JsonProperty("yearlyRemuneration") Integer yearlyRemuneration){}
+@JvmRecord
+data class YearlyFigure(
+    val year: @Min(1L) @Max(6L) Int?,
+    val salary: Int,
+    val yearlyRemuneration: Int
+)
