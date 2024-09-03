85b71d709662a8727e50217f0c94e8b128bec2ef
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-09-03 14:31:00 +0100

    Added service for Contract Calculator functionality as Kotlin project.

--- service/src/main/kotlin/org/wcl/mfn/service/tools/ContractCalculatorService.kt
+++ service/src/main/kotlin/org/wcl/mfn/service/tools/ContractCalculatorService.kt
@@ -1,8 +1,9 @@
 package org.wcl.mfn.service.tools
 
 import org.springframework.stereotype.Service
-import org.wcl.mfn.entities.contract.calculator.SuggestedContract
+import org.wcl.mfn.entities.contract.calculator.*
 import org.wcl.mfn.exceptions.validation.InvalidParameterException
+import kotlin.math.*
 
 @Service
 class ContractCalculatorService {
@@ -13,6 +14,48 @@ class ContractCalculatorService {
         if (requestedBonus <= 0) {
             throw InvalidParameterException("Invalid requested bonus - cannot be negative or zero");
         }
-        return ArrayList()
+
+        return generateSuggestedContracts(requestedSalary, requestedBonus)
+    }
+
+    private fun generateSuggestedContracts(requestedSalary: Int, requestedBonus: Int): List<SuggestedContract> {
+        val yearsList = IntRange(2, 6).toList()
+        return yearsList.stream().map { n -> generateSuggestedContract(n, requestedSalary, requestedBonus) }.toList()
+    }
+
+    private fun generateSuggestedContract(numberOfYears: Int, requestedSalary: Int, requestedBonus: Int): SuggestedContract {
+        val bonusPerYear = ceil((requestedBonus / numberOfYears.toDouble())).toInt()
+        val startingSalary = generateStartingSalary(requestedSalary, requestedBonus, bonusPerYear)
+        val yearlyFigures = generateYearlyFigures(numberOfYears, startingSalary, bonusPerYear)
+
+        return SuggestedContract(
+            numberOfYears,
+            yearlyFigures.stream().map { yf -> yf.salary }.toList().sum(),
+            bonusPerYear,
+            yearlyFigures.stream().map { yf -> yf.yearlyRemuneration }.toList().sum(),
+            yearlyFigures
+        )
+    }
+
+    private fun generateStartingSalary(requestedSalary: Int, requestedBonus: Int, bonusPerYear: Int): Int {
+        return if (requestedSalary > requestedBonus) {
+            requestedSalary
+        } else {
+            ceil(bonusPerYear * 0.75).toInt()
+        }
+    }
+
+    private fun generateYearlyFigures(numberOfYears: Int, startingSalary: Int, bonusPerYear: Int): List<YearlyFigure> {
+        return IntRange(1, numberOfYears).toList().stream()
+            .map { n -> generateYearlyFigure(n, startingSalary, bonusPerYear) }.toList()
+    }
+
+    private fun generateYearlyFigure(year: Int, startingSalary: Int, bonusPerYear: Int): YearlyFigure {
+        val salary: Int = if (year == 1) {
+            startingSalary
+        } else {
+            ceil(startingSalary.toDouble() * (1.05.pow(year - 1))).toInt()
+        }
+        return YearlyFigure(year, salary, salary + bonusPerYear)
     }
 }
--- service/src/test/kotlin/org/wcl/mfn/service/tools/ContractCalculatorServiceTest.kt
+++ service/src/test/kotlin/org/wcl/mfn/service/tools/ContractCalculatorServiceTest.kt
@@ -1,13 +1,14 @@
 package org.wcl.mfn.service.tools
 
-import org.junit.jupiter.api.*
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+import org.wcl.mfn.entities.contract.calculator.*
 import org.wcl.mfn.exceptions.validation.InvalidParameterException
 import kotlin.test.assertFailsWith
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.CoreMatchers.equalTo
 
 class ContractCalculatorServiceTest {
-    private val service:ContractCalculatorService= ContractCalculatorService();
+    private val service: ContractCalculatorService = ContractCalculatorService();
 
     @Test
     fun whenNegativeSalaryProvided_thenThrowException() {
@@ -43,5 +44,66 @@ class ContractCalculatorServiceTest {
         }
 
         assertThat(thrownException.message, equalTo("Invalid requested bonus - cannot be negative or zero"))
+    }
+
+    @Test
+    fun whenValidParametersProvided_thenSuggestedContractsAreGenerated() {
+        val suggestedContracts = service.suggestedContracts(564335, 3452267)
+
+        assertThat(suggestedContracts, notNullValue())
+        assertThat(suggestedContracts.size, equalTo(5))
+
+        val expectedContracts = generateExpectedSuggestedContracts();
+        assertThat(suggestedContracts, equalTo(expectedContracts))
+    }
+
+    private fun generateExpectedSuggestedContracts(): List<SuggestedContract> {
+        return listOf(
+            SuggestedContract(
+                2, 2653933, 1726134, 6106201,
+                listOf(
+                    YearlyFigure(1, 1294601, 3020735),
+                    YearlyFigure(2, 1359332, 3085466)
+                )
+            ),
+            SuggestedContract(
+                3, 2720820, 1150756, 6173088,
+                listOf(
+                    YearlyFigure(1, 863067, 2013823),
+                    YearlyFigure(2, 906221, 2056977),
+                    YearlyFigure(3, 951532, 2102288)
+                )
+            ),
+            SuggestedContract(
+                4, 2789950, 863067,  6242218,
+                listOf(
+                    YearlyFigure(1, 647301, 1510368),
+                    YearlyFigure(2, 679667, 1542734),
+                    YearlyFigure(3, 713650, 1576717),
+                    YearlyFigure(4, 749332, 1612399)
+                )
+            ),
+            SuggestedContract(
+                5, 2861400,  690454, 6313670,
+                listOf(
+                    YearlyFigure(1, 517841, 1208295),
+                    YearlyFigure(2, 543734, 1234188),
+                    YearlyFigure(3, 570920, 1261374),
+                    YearlyFigure(4, 599466, 1289920),
+                    YearlyFigure(5, 629439, 1319893)
+                )
+            ),
+            SuggestedContract(
+                6, 2935259, 575378,6387527 ,
+                listOf(
+                    YearlyFigure(1, 431534, 1006912),
+                    YearlyFigure(2, 453111, 1028489),
+                    YearlyFigure(3, 475767, 1051145),
+                    YearlyFigure(4, 499555, 1074933),
+                    YearlyFigure(5, 524533, 1099911),
+                    YearlyFigure(6, 550759, 1126137)
+                )
+            )
+        )
     }
 }
