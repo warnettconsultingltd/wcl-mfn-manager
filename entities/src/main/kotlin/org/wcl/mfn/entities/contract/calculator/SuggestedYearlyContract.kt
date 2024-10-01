0efecaf2a945809575cefb4543583a723a96154f
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-10-01 16:04:45 +0100

    Amended data class name

--- entities/src/main/kotlin/org/wcl/mfn/entities/contract/calculator/SuggestedYearlyContract.kt
+++ entities/src/main/kotlin/org/wcl/mfn/entities/contract/calculator/SuggestedYearlyContract.kt
@@ -3,7 +3,7 @@ package org.wcl.mfn.entities.contract.calculator
 import jakarta.validation.constraints.*
 
 @JvmRecord
-data class YearlyFigure(
+data class SuggestedYearlyContract(
     val year: @Min(1L) @Max(6L) Int?,
     val salary: Int,
     val yearlyRemuneration: Int
