1fa1329e5bb9665b5f630ac451e6c16326f6a23f
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-10-07 16:02:55 +0100

    Added forms for UI

---
+++ ui/src/main/kotlin/org/wcl/mfn/ui/controller/forms/contractcalculator/RequestedRemunerationForm.kt
@@ -0,0 +1,4 @@
+package org.wcl.mfn.ui.controller.forms.contractcalculator
+
+class RequestedRemunerationForm(var salary: Int = 0 , var bonus: Int = 0, var escalator: SalaryEscalatorForm) {
+}
---
+++ ui/src/main/kotlin/org/wcl/mfn/ui/controller/forms/contractcalculator/SalaryEscalatorForm.kt
@@ -0,0 +1,7 @@
+package org.wcl.mfn.ui.controller.forms.contractcalculator
+
+class SalaryEscalatorForm(var value:Int = 0) {
+    val defaultValue: Int = 5
+    val maxValue: Int = 100
+    val step: Int = 5
+}
