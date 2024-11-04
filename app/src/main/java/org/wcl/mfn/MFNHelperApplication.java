700dc6badc8208f2f8113c08b3f073164a8aedc6
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-11-04 11:19:33 +0000

    Added main application class to app module

---
+++ app/src/main/java/org/wcl/mfn/MFNHelperApplication.java
@@ -0,0 +1,14 @@
+package org.wcl.mfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication(scanBasePackages = {"org.wcl.mfn"})
+@EnableWebMvc
+public class MFNHelperApplication {
+    public static void main(String... args) {
+        SpringApplication.run(MFNHelperApplication.class, args);
+    }
+}
