c52434298712b9546df2ac519fbdc0437cafca6b
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-11-05 16:18:23 +0000

    Moved Context Smoke Test from ui into app module

---
+++ app/src/test/java/org/wcl/mfn/integration/ContextSmokeTest.java
@@ -0,0 +1,42 @@
+package org.wcl.mfn.integration;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.wcl.mfn.ui.controller.HomeController;
+import org.wcl.mfn.ui.controller.tools.ContractCalculatorUIController;
+
+@SpringBootTest()
+@ContextConfiguration(classes = {HomeController.class, ContractCalculatorUIController.class})
+public class ContextSmokeTest {
+    @Autowired
+    private HomeController homePageController;
+
+    @Autowired
+    private ContractCalculatorUIController contractCalculatorController;
+
+    @Test
+    public void checkThatContextLoads() {
+        assertThat(homePageController).isNotNull();
+        assertThat(contractCalculatorController).isNotNull();
+    }
+}
+
+//
+//@SpringBootTest()
+//@ContextConfiguration(classes = [HomeController::class, ContractCalculatorUIController::class])
+//class ContextSmokeTest {
+//    @Autowired
+//    private val homePageController: HomeController? = null
+//
+//    @Autowired
+//    private val contractCalculatorController: ContractCalculatorUIController? = null
+//
+//    @Test
+//    fun contextLoads() {
+//        assertThat(homePageController).isNotNull()
+//        assertThat(contractCalculatorController).isNotNull()
+//    }
+//}
