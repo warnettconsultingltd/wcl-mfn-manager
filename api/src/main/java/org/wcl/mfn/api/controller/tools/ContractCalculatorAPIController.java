544e713c6f7ca977d17a6fa3585f45e4f0806c81
Martin Warnett <coding.warnettconsultingltd@gmail.com>
2024-11-14 21:11:04 +0000

    Updated Contract Calculator API controller

---
+++ api/src/main/java/org/wcl/mfn/api/controller/tools/ContractCalculatorAPIController.java
@@ -0,0 +1,33 @@
+package org.wcl.mfn.api.controller.tools;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.wcl.mfn.entities.contract.calculator.*;
+import org.wcl.mfn.exceptions.validation.InvalidParameterException;
+import org.wcl.mfn.service.tools.ContractCalculatorService;
+
+import java.util.List;
+
+@RestController
+public class ContractCalculatorAPIController {
+    private final ContractCalculatorService service;
+
+    public ContractCalculatorAPIController(@Autowired final ContractCalculatorService service) {
+        this.service = service;
+    }
+
+    @PostMapping(value = "/api/contract-calculator",
+                 produces = MediaType.APPLICATION_JSON_VALUE,
+                 consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<SuggestedContract>> calculateSuggestedContracts (
+            @RequestBody @NotNull RequestedRemuneration requestedRemuneration) throws InvalidParameterException {
+        if (requestedRemuneration.salary() == 0 || requestedRemuneration.bonus() == 0) {
+            throw new InvalidParameterException("MFN-CONTRACT-001 : Salary and Bonus must be positive, non-zero values");
+        }
+
+        return ResponseEntity.ok(service.suggestedContracts(requestedRemuneration.salary(),
+                requestedRemuneration.bonus()));
+    }
+}
